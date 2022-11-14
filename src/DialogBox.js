d4f657777046a5ef190d6e0b2bd6dd3177c0b334
mikekim-fullstack <mikekim.fullstack@gmail.com>
2022-11-14 11:52:30 -0500

    the bialog box updated

--- src/DialogBox.js
+++ src/DialogBox.js
@@ -1,13 +1,9 @@
 import './DialogBox.css'
 function DialogBox({ message, onDialog, itemName }) {
-    const clickedContent = (e) => {
-        console.log('clickedContent')
-        e.stopPropagation()
-    }
+
     return (
         <div className='dialogbox__view' onClick={() => onDialog(false)} >
-            {/* <div className='content' onClick={(e) => e.stopPropagation()} > */}
-            <div className='content' onClick={(e) => clickedContent(e)} >
+            <div className='content' onClick={(e) => e.stopPropagation()} >
                 <div className='close' onClick={() => onDialog(false)}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6.4 18.65 5.35 17.6l5.6-5.6-5.6-5.6L6.4 5.35l5.6 5.6 5.6-5.6 1.05 1.05-5.6 5.6 5.6 5.6-1.05 1.05-5.6-5.6Z" /></svg>
                 </div>
